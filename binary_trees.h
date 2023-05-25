6a55402291a9418c4919652ea9db72f772983ee8
raed <raed.da3k@gmail.com>
2023-05-25 12:52:52 +0100

    headerfile

---
+++ binary_trees.h
@@ -0,0 +1,29 @@
+#ifndef BINARY_TREES_H
+#define BINARY_TREES_H
+
+/* Basic Binary Tree */
+typedef struct binary_tree_s
+{
+    int n;
+    struct binary_tree_s *parent;
+    struct binary_tree_s *left;
+    struct binary_tree_s *right;
+} binary_tree_t;
+
+/* Binary Search Tree */
+typedef struct binary_tree_s bst_t;
+
+/* AVL Tree */
+typedef struct binary_tree_s avl_t;
+
+/* Max Binary Heap */
+typedef struct binary_tree_s heap_t;
+
+/* Function prototypes */
+int binary_tree_balance(const binary_tree_t *tree);
+int binary_tree_is_full(const binary_tree_t *tree);
+int binary_tree_is_perfect(const binary_tree_t *tree);
+binary_tree_t *binary_tree_sibling(binary_tree_t *node);
+binary_tree_t *binary_tree_uncle(binary_tree_t *node);
+
+#endif /* BINARY_TREES_H */
